7b87bb57e5bc47702b7700e249aeff681921f9b0
Анатолий Ершов <a.v.ershov@sgugit.ru>
2021-11-18 19:26:35 +0700

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LoopFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N > 0:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int S = 0;
+            for (int i = 1; i <= 2 * N - 1; i = i + 2)
+            {
+                S = S + i;
+                Console.WriteLine(S);
+            }
+            Console.ReadKey();
+        }
+    }
+}
